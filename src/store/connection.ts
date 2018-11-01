71f75501a6dc0508324e5a451dd759f6c09fb3f8
blackdahila <ola.zxcvbnm@gmail.com>
2018-11-01 21:23:28 +0100

    Fix canceling connection issue

--- src/store/connection.ts
+++ src/store/connection.ts
@@ -1,8 +1,31 @@
-import mysql from 'mysql';
+import mysql, { Connection } from 'mysql';
 
-export const connection = mysql.createConnection({
+const dbConfig = {
   database: process.env.DB_NAME,
   host: process.env.DB_HOST,
   password: process.env.DB_PASSWORD,
   user: process.env.DB_USER,
-});
+};
+
+let connection: Connection;
+
+const handleDisconnect = () => {
+  connection = mysql.createConnection(dbConfig);
+  connection.connect(err => {
+    if (err) {
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000);
+    }
+  });
+  connection.on('error', err => {
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+};
+
+handleDisconnect();
+
+export { connection };
