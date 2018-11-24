747381f03b2a371831889d5349036c6e0a3f42c4
blackdahila <ola.zxcvbnm@gmail.com>
2018-11-24 15:26:20 +0100

    Add authfetch to group api

--- client/src/api/groupApi.ts
+++ client/src/api/groupApi.ts
@@ -1,18 +1,18 @@
 import { ROUTES, UserDTO } from '../../../common/api';
 
 import { SERVER_URL } from '.';
+import { authFetch } from './utils';
 
 const { GROUPS } = ROUTES;
 
 export const uploadUsers = async (fileContent: string): Promise<{ message: UserDTO[] }> => {
-  // TO DO
-  const response = await fetch(SERVER_URL + GROUPS.upload, {
+  const options = {
     body: JSON.stringify({ data: fileContent, group: 'test' }),
     headers: {
       Accept: 'application/json, text/plain, */*',
-      'Content-Type': 'application/json',
     },
     method: 'POST',
-  });
-  return response.json();
+  };
+
+  return authFetch<{ message: UserDTO[] }>(SERVER_URL + GROUPS.upload, options);
 };
