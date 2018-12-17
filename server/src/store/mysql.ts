b9a873941cc39e85b7f5f576b7f296912e9c874f
blackdahila <ola.zxcvbnm@gmail.com>
2018-12-17 15:59:56 +0100

    Extract update user to /store

--- server/src/store/mysql.ts
+++ server/src/store/mysql.ts
@@ -1,10 +1,12 @@
-import { MysqlError } from 'mysql';
+import { MysqlError, queryCallback } from 'mysql';
 
 import { UserDTO } from '../../../common/api';
 
 import { connection } from './connection';
 
-export const addUser = (user: UserDTO, callback: (err: MysqlError | null, res: any) => void) =>
+type Callback = queryCallback;
+
+export const addUser = (user: UserDTO, callback: Callback) =>
   connection.query(
     {
       sql: `
@@ -19,5 +21,23 @@ export const addUser = (user: UserDTO, callback: (err: MysqlError | null, res: a
     `,
       values: [user.user_name, user.email, user.user_role, user.student_index],
     },
-    (err, res) => callback(err, res)
+    callback
+  );
+
+export const updateUser = (user: UserDTO, callback: Callback) =>
+  connection.query(
+    {
+      sql: `
+    UPDATE
+      users
+    SET
+      email = ?,
+      user_name = ?,
+      user_role = ?,
+      student_index = ?
+    WHERE id = ?;
+      `,
+      values: [user.email, user.user_name, user.user_role, user.student_index, user.id],
+    },
+    callback
   );
