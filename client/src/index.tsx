f6c134bd1cee8d01bf068354179d209b671ebc2a
hasparus <hasparus@gmail.com>
2018-12-30 20:07:17 +0100

    Clean up

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-window.process = process || {};
-process.env = process.env || {};
-process.env['NODE' + 'ENV'] = process.env.NODE_ENV;
-
-const { App } = require('./App');
+import { App } from './App';
 
 render(<App />, document.getElementById('root'));
