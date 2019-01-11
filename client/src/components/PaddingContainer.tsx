db460d86216ae620da4f3d4a01234cf132822ac0
blackdahila <ola.zxcvbnm@gmail.com>
2019-01-11 17:35:59 +0100

    Update paddingContainer

--- client/src/components/PaddingContainer.tsx
+++ client/src/components/PaddingContainer.tsx
@@ -2,5 +2,5 @@ import styled from '@emotion/styled';
 
 import { Theme } from './theme';
 export const PaddingContainer = styled.div`
-  padding: ${Theme.Padding.Half} ${Theme.Padding.Standard} 0 ${Theme.Padding.Standard};
+  padding: ${Theme.Padding.Half} ${Theme.Padding.Standard};
 `;
