02b12fe0e6a41775e295788453f1e008749c9d12
blackdahila <ola.zxcvbnm@gmail.com>
2019-02-02 19:16:53 +0100

    Add class_number to groups table

--- packages/common/api.ts
+++ packages/common/api.ts
@@ -82,6 +82,7 @@ export type GroupDTO = {
   group_type: GroupType;
   lecturer_id: UserDTO['id'];
   lecturer_name?: string;
+  class_number?: number;
   academic_year?: string;
   data?: Record<string, unknown>;
 };
