8d9b543c71f7678884ee66a78b88c0c9d3f21d04
blackdahila <ola.zxcvbnm@gmail.com>
2019-03-09 09:48:41 +0100

    Remove tests section

--- packages/client/src/containers/Groups/sections/TestsSection.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-
-import { GroupApiContextState } from '../GroupApiContext';
-
-type Props = GroupApiContextState;
-export const TestsSection = (props: Props) => <div />;
