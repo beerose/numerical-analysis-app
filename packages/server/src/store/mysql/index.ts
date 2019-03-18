875c365821109599d47bb9017e00335d50813e67
blackdahila <ola.zxcvbnm@gmail.com>
2019-03-18 22:01:14 +0100

    Move setActivity do meetings/

--- packages/server/src/store/mysql/index.ts
+++ packages/server/src/store/mysql/index.ts
@@ -1,32 +1,5 @@
-import { MeetingDTO, UserDTO } from 'common';
-import { queryCallback as Callback } from 'mysql';
-
-import { connection } from '../connection';
-
 export * from './users';
 export * from './meetings';
 export * from './groups';
 export * from './presences';
 export * from './grades';
-
-export const setActivity = (
-  {
-    userId,
-    meetingId,
-    points,
-  }: { userId: UserDTO['id']; meetingId: MeetingDTO['id']; points: number },
-  callback: Callback
-) =>
-  connection.query(
-    {
-      sql: /* sql */ `
-        INSERT INTO
-          user_was_active_in_meeting(user_id, meeting_id, points)
-        VALUES
-          (?, ?, ?)
-        ON DUPLICATE KEY UPDATE
-          points = VALUES(points)`,
-      values: [userId, meetingId, points],
-    },
-    callback
-  );
--- packages/server/src/store/mysql/meetings.ts
+++ packages/server/src/store/mysql/meetings.ts
@@ -95,3 +95,25 @@ export const removeStudentFromGroup = (
     },
     callback
   );
+
+export const setActivity = (
+  {
+    userId,
+    meetingId,
+    points,
+  }: { userId: UserDTO['id']; meetingId: MeetingDTO['id']; points: number },
+  callback: QueryCallback
+) =>
+  connection.query(
+    {
+      sql: /* sql */ `
+          INSERT INTO
+            user_was_active_in_meeting(user_id, meeting_id, points)
+          VALUES
+            (?, ?, ?)
+          ON DUPLICATE KEY UPDATE
+            points = VALUES(points)`,
+      values: [userId, meetingId, points],
+    },
+    callback
+  );
