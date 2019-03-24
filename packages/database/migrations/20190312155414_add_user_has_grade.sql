17d86f78615eebfdb6a2adfbaa23a52e1c24a2be
blackdahila <ola.zxcvbnm@gmail.com>
2019-03-24 13:08:08 +0100

    Update migration, add user_has_final_grade

--- packages/database/migrations/20190312155414_add_user_has_grade.sql
+++ packages/database/migrations/20190312155414_add_user_has_grade.sql
@@ -6,5 +6,14 @@ CREATE TABLE user_has_points (
   UNIQUE KEY(user_id, task_id)
 );
 
+CREATE TABLE user_has_final_grade (
+  user_id INT NOT NULL REFERENCES users(id),
+  group_id INT NOT NULL REFERENCES `groupd`(id),
+  grade INT
+);
+
 -- migrate:down
 DROP TABLE user_has_points;
+DROP TABLE user_has_final_grade;
+
+
