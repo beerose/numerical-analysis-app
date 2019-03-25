89793da76b0becfa64e3f7873887db0a175df5d2
blackdahila <ola.zxcvbnm@gmail.com>
2019-03-25 18:44:56 +0100

    Add db View for users meetings results

--- packages/database/migrations/20190312155414_add_user_has_grade.sql
+++ packages/database/migrations/20190312155414_add_user_has_grade.sql
@@ -12,6 +12,18 @@ CREATE TABLE user_has_final_grade (
   grade INT
 );
 
+CREATE VIEW user_activities_and_presences AS SELECT
+	uam.user_id,
+	count(uam.meeting_id) AS presences,
+	sum(uwa.points) AS activity,
+	m.group_id
+FROM
+	user_attended_in_meeting uam
+	LEFT JOIN user_was_active_in_meeting uwa ON (uam.user_id = uwa.user_id AND uam.meeting_id = uwa.meeting_id)
+	JOIN meetings m on (uam.meeting_id = m.id)
+	GROUP BY
+		uam.user_id, m.group_id;
+
 -- migrate:down
 DROP TABLE user_has_points;
 DROP TABLE user_has_final_grade;
