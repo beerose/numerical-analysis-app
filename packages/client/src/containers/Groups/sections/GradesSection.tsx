2e98d63a24e5fce959668006bd2b33dcf2f497ef
blackdahila <ola.zxcvbnm@gmail.com>
2019-03-26 15:23:15 +0100

    Set suggested grade based on results and tresholds

--- packages/client/src/containers/Groups/sections/GradesSection.tsx
+++ packages/client/src/containers/Groups/sections/GradesSection.tsx
@@ -3,11 +3,13 @@ import { RouteComponentProps } from 'react-router';
 
 import { GroupApiContextState } from '../GroupApiContext';
 import { Flex, Table, Theme } from '../../../components';
-import { Input, Select } from 'antd';
+import { Select, Spin } from 'antd';
 import {
   UserDTO,
   UserResultsDTO,
   ApiResponse,
+  UserWithGroups,
+  GroupDTO,
 } from '../../../../../../dist/common';
 import { tresholdsKeys } from '../components/GradeTresholdsList';
 
@@ -16,10 +18,30 @@ type TableDataItem = {
   userName: UserDTO['user_name'];
   index: UserDTO['student_index'];
   finalGrade?: number;
-  suggestedGrade: number;
   tasksPoints: number;
   maxTasksPoints: number;
-  meetingsPoints: number;
+  presences: number;
+  activity: number;
+};
+
+const mergedResultsToTableItem = (
+  groupId: GroupDTO['id'],
+  student: UserWithGroups,
+  results?: UserResultsDTO
+) => {
+  const final = student.groups_grades
+    ? student.groups_grades.find(g => g.group_id === groupId)
+    : undefined;
+  return {
+    userId: student.id,
+    userName: student.user_name,
+    index: student.student_index,
+    finalGrade: final && final.grade,
+    tasksPoints: results ? results.tasks_grade : 0,
+    maxTasksPoints: results ? results.max_tasks_grade : 0,
+    presences: results ? results.presences : 0,
+    activity: results ? results.sum_activity : 0,
+  };
 };
 
 const SetGrade = ({
@@ -88,26 +110,48 @@ export const GradesSection = (props: Props) => {
         setUsersResults(res);
       });
     }
+
     if (currentGroupStudents && currentGroup && usersResults) {
       const data = currentGroupStudents.map(s => {
-        const final = s.groups_grades
-          ? s.groups_grades.find(g => g.group_id === currentGroup.id)
-          : undefined;
         const results = usersResults.find(r => r.user_id === s.id);
-        return {
-          userId: s.id,
-          userName: s.user_name,
-          index: s.student_index,
-          finalGrade: final && final.grade,
-          suggestedGrade: 5, // TO DO
-          tasksPoints: results ? results.tasks_grade : 0,
-          maxTasksPoints: results ? results.max_tasks_grade : 0,
-          meetingsPoints: results ? results.presences : 0, // TO DO
-        };
+        return mergedResultsToTableItem(currentGroup.id, s, results);
       });
       setTableData(data);
     }
   }, [usersResults, props.currentGroupStudents]);
+
+  const getSuggestedGrade = (
+    tasksPoints: number,
+    maxTasksPoints: number,
+    _presences: number,
+    _activity: number
+  ) => {
+    if (!props.currentGroup || !props.currentGroup.data) {
+      return <Spin />;
+    }
+    const tresholds = props.currentGroup.data.tresholds;
+    if (!tresholds) {
+      return 'Brak odpowiednich ustawień';
+    }
+
+    // TO DO: add meetings points
+    const pointsPercentage = (tasksPoints / maxTasksPoints) * 100;
+
+    switch (true) {
+      case pointsPercentage >= tresholds[5]:
+        return 5;
+      case pointsPercentage >= tresholds['4.5']:
+        return 4.5;
+      case pointsPercentage >= tresholds[4]:
+        return 4;
+      case pointsPercentage >= tresholds['3.5']:
+        return 3.5;
+      case pointsPercentage >= tresholds[3]:
+        return 3;
+      default:
+        return 2;
+    }
+  };
 
   const columns = [
     { title: 'Imię i nazwisko', dataIndex: 'userName', key: 'name' },
@@ -128,7 +172,7 @@ export const GradesSection = (props: Props) => {
       key: 'meetings_grade',
       width: 120,
       render: (item: TableDataItem) => (
-        <Flex justifyContent="center">{item.meetingsPoints}</Flex>
+        <Flex justifyContent="center">{item.presences + item.activity}</Flex>
       ),
     },
     {
@@ -137,7 +181,12 @@ export const GradesSection = (props: Props) => {
       width: 100,
       render: (item: TableDataItem) => (
         <Flex justifyContent="center" fontWeight="bold">
-          {item.suggestedGrade}
+          {getSuggestedGrade(
+            item.tasksPoints,
+            item.maxTasksPoints,
+            item.presences,
+            item.activity
+          )}
         </Flex>
       ),
     },
