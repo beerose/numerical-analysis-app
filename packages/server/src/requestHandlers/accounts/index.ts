21a4797c906ecad6eb2baadd3c78ffe2ca8eb3b5
blackdahila <ola.zxcvbnm@gmail.com>
2019-03-28 13:33:08 +0100

    Add endpoint for change password

--- packages/server/src/requestHandlers/accounts/index.ts
+++ packages/server/src/requestHandlers/accounts/index.ts
@@ -1,12 +1,19 @@
-import { ServerRoutes } from 'common';
+import { ServerRoutes, UserRole } from 'common';
 import { Router } from 'express';
+import { auth } from '../../middleware';
 
 const { Accounts: Routes } = ServerRoutes;
 
 import { loginUser } from './loginUser';
 import { checkNewAccountToken, storeUserPassword } from './newAccount';
+import { changePassword } from './changePassword';
 
 export const router = Router();
 
 router.post(Routes.New, checkNewAccountToken, storeUserPassword);
 router.post(Routes.Login, loginUser);
+router.post(
+  Routes.ChangePassword,
+  auth.authorize([UserRole.admin, UserRole.student, UserRole.superUser]),
+  changePassword
+);
