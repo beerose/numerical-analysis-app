438c3167b23899d0ef78bf54a1bc26a07ad6a389
blackdahila <ola.zxcvbnm@gmail.com>
2019-03-28 13:31:04 +0100

    Add implementation for changing password

---
+++ packages/server/src/requestHandlers/accounts/changePassword.ts
@@ -0,0 +1,47 @@
+import { compare as comparePassword } from 'bcrypt';
+import { apiMessages, UserDTO } from 'common';
+import * as codes from 'http-status-codes';
+import * as t from 'io-ts';
+
+import {
+  BackendResponse,
+  generateUserJwtToken,
+  GetRequest,
+  handleBadRequest,
+} from '../../lib';
+import { db } from '../../store';
+import { storePassword } from './storePassword';
+
+const ChangePasswordBodyV = t.type({
+  new_password: t.string,
+});
+
+type ChangePasswordRequest = GetRequest<typeof ChangePasswordBodyV>;
+
+export const changePassword = (
+  req: ChangePasswordRequest,
+  res: BackendResponse
+) => {
+  handleBadRequest(ChangePasswordBodyV, req.body, res).then(() => {
+    const { new_password } = req.body;
+    const user = res.locals.user;
+
+    if (!user) {
+      res
+        .status(codes.INTERNAL_SERVER_ERROR)
+        .send({ error: apiMessages.internalError });
+      return;
+    }
+
+    storePassword(new_password, user, result => {
+      if ('error' in result) {
+        res
+          .status(result.code)
+          .send({ error: result.error, error_details: result.error_details });
+        return;
+      }
+      res.status(codes.OK).send({ message: apiMessages.passwordChanged });
+      return;
+    });
+  });
+};
