712caee3d348b14570c4f76d73a11523d558fffd
hasparus <hasparus@gmail.com>
2019-03-28 17:11:16 +0100

    Add deploy script

--- packages/server/scripts/deploy.ts
+++ packages/server/scripts/deploy.ts
@@ -3,7 +3,7 @@ import exec from 'executive';
 exec.sync(
   ` 
   scp -rv ../../dist/* anumuser@rno.ii.uni.wroc.pl:~/app
-  ssh anumuser@rno.ii.uni.wroc.pl
+  ssh anumuser@rno.ii.uni.wroc.pl "cd ~/app/server; yarn; pm2 start ./index.js --name lagrange-server"
 `,
   {
     syncThrows: true,
