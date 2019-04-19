dba65d7f90668a13b27c0d1359eb8dc4b5964526
hasparus <hasparus@gmail.com>
2019-04-19 07:09:03 +0200

    Add tslint-disable comment

--- packages/client/src/components/Sandbox.tsx
+++ packages/client/src/components/Sandbox.tsx
@@ -2,5 +2,6 @@ import React, { ComponentProps } from 'react';
 
 type SandboxProps = Required<Pick<ComponentProps<'iframe'>, 'srcDoc'>>;
 export const Sandbox: React.FC<SandboxProps> = ({ srcDoc }) => (
+  // tslint:disable-next-line:react-a11y-iframes
   <iframe sandbox="allow-scripts" srcDoc={srcDoc} style={{ display: 'none' }} />
 );
