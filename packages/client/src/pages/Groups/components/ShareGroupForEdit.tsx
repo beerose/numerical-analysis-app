0924bb86d067ddff402dcc3a59af8d91e4528eee
blackdahila <ola.zxcvbnm@gmail.com>
2019-05-22 11:38:17 +0200

    Add Select for lecturers in share for edit'

---
+++ packages/client/src/pages/Groups/components/ShareGroupForEdit.tsx
@@ -0,0 +1,48 @@
+import { Button, Icon, Popover, Spin, Tooltip } from 'antd';
+import React, { useEffect, useState } from 'react';
+
+import { UserDTO } from '../../../../../../dist/common';
+import { GroupApiContextState } from '../GroupApiContext';
+
+import { SelectLecturer } from './SelectLecturer';
+
+type PopoverContentProps = {
+  lecturers?: UserDTO[];
+};
+const PopoverContent = ({ lecturers }: PopoverContentProps) =>
+  lecturers ? <SelectLecturer lecturers={lecturers} /> : <Spin />;
+
+type ShareGroupForEditProps = Pick<
+  GroupApiContextState,
+  'lecturers' | 'actions'
+>;
+export const ShareGroupForEdit = ({
+  lecturers,
+  actions,
+}: ShareGroupForEditProps) => {
+  const [formVisible, setFormVisible] = useState(false);
+
+  useEffect(() => {
+    if (!lecturers) {
+      actions.listLecturers();
+    }
+  }, [lecturers]);
+
+  return (
+    <Popover
+      style={{ width: 400 }}
+      content={<PopoverContent lecturers={lecturers} />}
+      title="Wybierz z listy użytkowników"
+      trigger="click"
+      visible={formVisible}
+      onVisibleChange={() => setFormVisible(true)}
+      placement="right"
+    >
+      <Tooltip title="Udostępnij grupę do edycji innemu użytkownikowi">
+        <Button onClick={() => setFormVisible(true)} style={{ width: '90%' }}>
+          Udostępnij
+        </Button>
+      </Tooltip>
+    </Popover>
+  );
+};
