def90431ac50e5491ddae819a6f1d17dd8c5ee78
blackdahila <ola.zxcvbnm@gmail.com>
2019-06-09 14:27:41 +0200

    Remove obsolete function

--- packages/client/src/components/IfUserPrivileged.tsx
+++ packages/client/src/components/IfUserPrivileged.tsx
@@ -1,32 +1,7 @@
 import { UserPrivileges, UserRole } from 'common';
 import React from 'react';
 
-import { useAuthStore } from '../AuthStore';
-
-export const isUserPrivileged = (
-  to: UserPrivileges.What[],
-  resource: UserPrivileges.Where,
-  withId: number
-): boolean => {
-  const user = useAuthStore(state => state.user);
-
-  if (user && user.user_role === UserRole.Admin) {
-    return true;
-  }
-
-  if (!user || !user.privileges) {
-    return false;
-  }
-
-  const privilegesSection = user.privileges[resource];
-  if (!privilegesSection || !privilegesSection[withId]) {
-    return false;
-  }
-
-  return to.every(what => privilegesSection[withId].includes(what))
-    ? true
-    : false;
-};
+import { isUserPrivileged } from '../utils/isUserPrivileged';
 
 type IfUserPrivilegedProps = {
   to: UserPrivileges.What[];
