cd767dbd5ac91dc870a6e187ac0898c089afc6e4
hasparus <hasparus@gmail.com>
2019-08-30 07:53:58 +0200

    Add PUBLIC_URL to React Router basename

--- packages/scripts/build-and-deploy-client-prod.ts
+++ packages/scripts/build-and-deploy-client-prod.ts
@@ -1,9 +1,29 @@
 import { exec } from './exec';
 
+exec(
+  `
+  mv .env.production .env.production.backup
+  mv .env.production-baremetal .env.production
+`,
+  {
+    dir: 'packages/client',
+  }
+);
+
 exec(`
   rm -rf dist/client/
-  yarn build-client --public-url /lagrange --no-minify
+  yarn build-client --public-url /lagrange
 `);
+
+exec(
+  `
+  mv .env.production .env.production-baremetal
+  mv .env.production.backup .env.production
+`,
+  {
+    dir: 'packages/client',
+  }
+);
 
 exec(`
   ssh anumuser@rno.ii.uni.wroc.pl rm -rf ~/www-lagrange/*
