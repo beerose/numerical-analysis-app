3b675322dcdb0f37840302eab74ca23acad46b08
hasparus <hasparus@gmail.com>
2019-09-09 06:17:20 +0200

    Fix weird TS issue

--- packages/common/domain/User.ts
+++ packages/common/domain/User.ts
@@ -36,18 +36,18 @@ export enum UserRole {
 }
 
 export namespace UserRole {
-  export const All = [UserRole.Admin, UserRole.SuperUser, UserRole.Student];
-  export const NonStudents = [UserRole.Admin, UserRole.SuperUser];
-
-  const values: UserRole[] = Object.values(UserRole);
+  export const All: UserRole[] = [
+    UserRole.Admin,
+    UserRole.SuperUser,
+    UserRole.Student,
+  ];
+  export const NonStudents: UserRole[] = [UserRole.Admin, UserRole.SuperUser];
 
   export function assert(str: string): UserRole {
-    if (values.includes(str as UserRole)) {
+    if (All.includes(str as UserRole)) {
       return str as UserRole;
     }
-    throw new Error(
-      `${str} is not UserRole. Legal roles are ${values.join(' ')}`
-    );
+    throw new Error(`${str} is not UserRole. Legal roles are ${All.join(' ')}`);
   }
 }
 
