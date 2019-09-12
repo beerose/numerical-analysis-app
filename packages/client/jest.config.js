39a2154cd441a6ecad1daf3261f7b5c36f03f6d0
hasparus <hasparus@gmail.com>
2019-09-13 00:58:49 +0200

    Add browserlist to client/package.json

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -1,7 +1,5 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  // globalSetup: '<rootDir>/scripts/tests/setup.ts',
-  // globalTeardown: '<rootDir>/scripts/tests/teardown.ts',
   testPathIgnorePatterns: ['/node_modules/'],
 };
