dede50709e3c113eee963db31ad019138cce8b7a
hasparus <hasparus@gmail.com>
2019-09-13 00:59:23 +0200

    Remove unused import

--- packages/client/src/pages/Groups/components/EquationEvalSandbox.tsx
+++ packages/client/src/pages/Groups/components/EquationEvalSandbox.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 
 import { Sandbox } from '../../../components/Sandbox';
 import { usePostMessageHandler } from '../../../utils/usePostMessageHandler';
