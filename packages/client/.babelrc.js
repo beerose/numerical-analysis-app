8b5a645f37cac947b4c4b879b601e7128dea9de5
hasparus <hasparus@gmail.com>
2019-09-13 00:57:09 +0200

    Clean up babel config

--- packages/client/.babelrc.js
+++ packages/client/.babelrc.js
@@ -1,32 +1,8 @@
 module.exports = {
-  // presets: [
-  //   [
-  //     '@emotion/babel-preset-css-prop',
-  //     {
-  //       autoLabel: true,
-  //       labelFormat: '[local]',
-  //     },
-  //   ],
-  // ],
   plugins: [
     'babel-plugin-transform-inline-environment-variables',
     '@emotion/babel-plugin-core',
     '@babel/plugin-transform-react-constant-elements',
-    // [
-    //   'babel-plugin-jsx-pragmatic',
-    //   {
-    //     module: '@emotion/core',
-    //     export: 'jsx',
-    //     import: 'jsx',
-    //   },
-    // ],
-    // [
-    //   'babel-plugin-transform-react-jsx',
-    //   {
-    //     pragma: 'jsx',
-    //     pragmaFrag: 'React.Fragment',
-    //   },
-    // ],
   ],
   env: {
     test: {
